650c4db829b31319671bc73add60ebf7cf3042d6
Christine Jou <chriskjou@gmail.com>
2017-05-25 18:07:18 -0500

    dependent submit form

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#gratitude").hide();
+  $("input[name=form-type]").click(function()
+    {
+        if ( $("#form-story").attr('checked'))
+            $("#gratitude").hide();
+        if ( $("#form-letter").attr('checked'))
+            $("#gratitude").show();
+    });
+});
