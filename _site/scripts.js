ca3e1431767e94e731689984587d8f4dbaa0c954
Christine Jou <chriskjou@gmail.com>
2017-05-25 21:07:09 -0500

    formspree for submit

--- _site/scripts.js
+++ _site/scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   $("#gratitude").hide();
   $("input[name=form-type]").click(function()
     {
-        if ( $("#form-story").attr('checked'))
-            $("#gratitude").hide();
+    		if ( $("#form-story").attr('checked'))
+        	$("#gratitude").hide();
         if ( $("#form-letter").attr('checked'))
-            $("#gratitude").show();
+           $("#gratitude").show();
     });
 });
