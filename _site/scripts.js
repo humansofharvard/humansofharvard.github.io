69d6d1848de1c76c3773d7115b46f496546feaf0
Christine Jou <christinejou@Christines-MacBook-Pro.local>
2017-05-25 21:59:39 -0500

    dependent info box for letter

--- _site/scripts.js
+++ _site/scripts.js
@@ -3,8 +3,14 @@ $(document).ready(function() {
   $("input[name=form-type]").click(function()
     {
     		if ( $("#form-story").attr('checked'))
+        {
         	$("#gratitude").hide();
+          $("#rest").show();
+        }
         if ( $("#form-letter").attr('checked'))
+        {
            $("#gratitude").show();
+           $("rest").show();
+         }
     });
 });
